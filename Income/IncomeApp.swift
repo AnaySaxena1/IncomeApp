3cf0d42a51ec35c626b6295df8f7afd107e7ea33
AnaySaxena <anaysaxena686@gmail.com>
2025-06-08 11:34:53 +0530

    Initial Commit

---
+++ Income/IncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp.swift
+//  Income
+//
+//  Created by Anay Saxena on 08/06/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
